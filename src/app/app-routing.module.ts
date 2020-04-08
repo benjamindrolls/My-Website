dfe84b15de6bb59283df7191207434f273218ec0
ben <benjamindrolls@gmail.com>
2020-04-08 14:41:05 -0400

    new redesign of structure of site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { MyProjectsComponent } from './home/my-projects/my-projects.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
   {path: "Projects", component: MyProjectsComponent},
-  {path: "", component:MyProjectsComponent}
+  {path: "Home", component:HomeComponent},
+  {path: "", component:HomeComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { MyProjectsComponent } from './home/my-projects/my-projects.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyProjectsComponent
+    MyProjectsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
